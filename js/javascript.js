dab0c3ace7eb4babe02af42e9eb0e6d85a4c91c8
HrAagesen <mikkel_aagesen@hotmail.com>
2018-03-14 12:13:33 +0100

    stort del, header mangler lidt, footer skal fixes meget

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,13 @@
-function myFunction() {
-    var x = document.getElementById("myDate").type;
-    document.getElementById("demo").innerHTML = x;
+// function myFunction() {
+//     var x = document.getElementById("myDate").type;
+//     document.getElementById("demo").innerHTML = x;
+// }
+function openNav(){
+  document.getElementById("mynav").style.width = "50%";
 }
+function closeNav(){ /*javascript til at lukke menuen når der trykkes på x */
+  document.getElementById("mynav").style.width = "0%";
+}
+function updateTextInput(val) {
+          document.getElementById('textInput').value=val;
+        }
