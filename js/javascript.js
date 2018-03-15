7d283f33e52776e5a2399c5e51205ff86a7cd6f0
HrAagesen <mikkel_aagesen@hotmail.com>
2018-03-15 13:27:37 +0100

    Done, med små fejl

--- js/javascript.js
+++ js/javascript.js
@@ -3,11 +3,8 @@
 //     document.getElementById("demo").innerHTML = x;
 // }
 function openNav(){
-  document.getElementById("mynav").style.width = "50%";
+  document.getElementById("mynav").style.width = "60%";
 }
 function closeNav(){ /*javascript til at lukke menuen når der trykkes på x */
   document.getElementById("mynav").style.width = "0%";
 }
-function updateTextInput(val) {
-          document.getElementById('textInput').value=val;
-        }
